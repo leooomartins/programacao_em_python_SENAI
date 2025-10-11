d1927894a1bc804c9017639b0f3551dc24a2ecb4
Leo <leonardomartins564@gmail.com>
2025-10-11 12:09:12 -0300

    Add files via upload

---
+++ aula 4/aula4.py
@@ -0,0 +1,100 @@
+# # sinais ariméticos 
+# # +    -    *   /   //    %    **0.5
+# # Saida é sempre um número
+
+# n1  = 10
+# n2  = 20
+
+# print(n1  +  n2 )
+# print(n1  -  n2 )
+# print(n1  *  n2 )
+# print(n1  /  n2 )
+
+# # primeiro valor decimal  
+# print(n1  //  n2 )
+
+# # resto da conta de divisão 
+# print(10  %   2 )
+
+# # sinais lógicos -  saída é sempre True ou False
+# n1  = 10
+# n2  = 2
+# print(n1 > n2)# maior
+# print(n1 < n2)# menor  
+# print(n1 >= n2)# maior ou igual
+# print(n1 <= n2)# menor ou igual
+# print(n1 != n2)# diferente
+# print(n1 == n2)# igual
+
+
+# print('objeto', 10, n1) # output -- saída
+
+# nome = input('digite seu nome: ') # input -- entrada
+
+# # o Input naturalmente gera um texto 
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 + n2)
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 - n2)
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 / n2)
+
+# n1  =  float(input('Digite um número: '))
+# n2  =  float(input('Digite outro número: '))
+# print(n1 * n2)
+
+
+# concatenação -- juntar dados
+
+# nome  =  input('Digite seu nome: ')
+# idade = int(input('Digite sua idade: '))
+# endereco = input('Digite seu endereço: ')
+# curso = input('Curso: ')
+# salario =  float(input('Digite seu salário: '))
+# # concatenar 
+# print('Nome', nome, 'idade: ',idade)
+# print(f'Nome {nome} idade {idade}')
+# print('Nome'+' '+nome+' '+ 'idade'+ ' ', idade)
+# print('Nome {} idade {}'.format(nome, idade))
+# print('Nome %s idade %d'%(nome, idade))
+
+#questão 1
+
+# n1 = int(input('Digite um número inteiro: '))
+# calc = n1 * n1
+#print(calc)
+
+#questão 2
+
+# PrimeiroN = input('Digite seu primeiro nome: ')
+# SegundoN = input('Digite seu segundo nome: ')
+
+# print(f'Seu nome é: {PrimeiroN} {SegundoN}'.format(PrimeiroN, SegundoN))
+
+#Questão 3
+
+# PN1 = input('Digite um número inteiro: ')
+# SN1 = input('Digite outro número inteiro: ')
+
+# print(f'Os números inteiros citados são: {PN1} e {SN1}'.format(PN1, SN1))
+
+#Questão 4
+
+# p1 = 'Python' 
+
+# p2 = int(input('Digite um número inteiro: '))
+
+# print(f'Programação em {p1} com a Carga Horária: {p2}'.format(p1, p2))
+
+# Questão 5
+
+# Frase = 'Palmeiras não tem '
+# Palavra = input('Digite a palavra para completar a frase: ')
+
+# print(Frase, Palavra, 'CHUPAAAAAAAAAAAAAAAAA PALMEIRASSS')
